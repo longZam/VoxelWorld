79876e439bf1a3b3d830a62022d04960df532a59
longZam <longzam305@gmail.com>
2023-12-27 06:58:25 +0900

    fix: 잘못된 결과를 만드는 좌표 변환 함수 수정

--- VoxelWorld.Core/World.cs
+++ VoxelWorld.Core/World.cs
@@ -5,7 +5,6 @@ namespace VoxelWorld.Core;
 
 public class World
 {
-    // 각 axis에 대해 정렬된 리스트를 만들고, 이진 탐색으로 탐색하기?
     private readonly IChunkLoader chunkLoader;
     private readonly Octree<Chunk> chunkTree;
     private readonly ReaderWriterLockSlim chunkReaderWriterLockSlim;
@@ -38,24 +37,34 @@ public class World
 
     public static Vector2Int WorldToRegion(Vector3Int worldPosition)
     {
-        worldPosition /= Region.BLOCK_CORNER;
-        return new(worldPosition.x, worldPosition.y);
+        return ChunkToRegion(WorldToChunk(worldPosition));
+    }
+
+    public static Vector2Int ChunkToRegion(Vector3Int chunkPosition)
+    {
+        return new Vector2Int(chunkPosition.x >> 4,
+                            chunkPosition.y >> 4);
     }
 
     public static Vector3Int WorldToChunk(Vector3Int worldPosition)
     {
-        return worldPosition / Chunk.CORNER;
+        return new Vector3Int(worldPosition.x >> 4,
+                            worldPosition.y >> 4,
+                            worldPosition.z >> 4);
     }
-
-    public static Vector3Int RegionToWorld(Vector2Int regionPosition)
+    
+    public static Vector3Int RegionToChunk(Vector2Int regionPosition)
     {
-        return new Vector3Int(regionPosition.x * Region.BLOCK_CORNER,
-                              regionPosition.y * Region.BLOCK_CORNER,
-                              0);
+        return new Vector3Int(regionPosition.x, regionPosition.y, 0) * Region.CHUNK_SIDE;
     }
 
     public static Vector3Int ChunkToWorld(Vector3Int chunkPosition)
     {
         return chunkPosition * Chunk.CORNER;
+    }
+
+    public static Vector3Int RegionToWorld(Vector2Int regionPosition)
+    {
+        return ChunkToWorld(RegionToChunk(regionPosition));
     }
 }
