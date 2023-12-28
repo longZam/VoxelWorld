d7aafa8e90b81fe729015f45b90d8cce348a45c7
longZam <longzam305@gmail.com>
2023-12-28 05:08:51 +0000

    chore: chunkloader 코드 주석 작성

--- VoxelWorld.Core/WorldLoaders/FileSystemChunkLoader.cs
+++ VoxelWorld.Core/WorldLoaders/FileSystemChunkLoader.cs
@@ -1,16 +1,20 @@
-
-
-
 using System.Collections.Concurrent;
 using VoxelWorld.Core.WorldLoaders;
 
 namespace VoxelWorld.Core;
 
 
+/// <summary>
+/// 로컬 컴퓨터의 파일 시스템에서 청크를 로딩하는 클래스
+/// </summary>
 public class FileSystemChunkLoader : IChunkLoader
 {
     private readonly FileSystemRegionLoader regionLoader;
     private readonly WorldGenerator worldGenerator;
+    
+    /// <summary>
+    /// 같은 내용의 중복 요청으로 인한 충돌을 방지하는 세마포어
+    /// </summary>
     private readonly ConcurrentDictionary<Vector3Int, SemaphoreSlim> requestSemaphores;
 
 
@@ -25,13 +29,15 @@ public class FileSystemChunkLoader : IChunkLoader
     {
         cancellationToken.ThrowIfCancellationRequested();
 
-
+        // 청크 위치로부터 region의 좌표를 계산
         Vector2Int regionPosition = World.ChunkToRegion(chunkPosition);
 
+        // region 내에서의 로컬 청크 좌표 계산
         int x = chunkPosition.x % Region.CHUNK_CORNER;
         int y = chunkPosition.y % Region.CHUNK_CORNER;
         int z = chunkPosition.z % Region.CHUNK_CORNER;
 
+        // 음수에 대한 로컬 좌표 처리
         if (x < 0)
             x += Region.CHUNK_CORNER;
         if (y < 0)
@@ -41,6 +47,7 @@ public class FileSystemChunkLoader : IChunkLoader
 
         Region result = await regionLoader.LoadRegionAsync(regionPosition, cancellationToken);
 
+        // 같은 요청의 중복 작업이 동시에 수행됨을 방지
         var semaphore = requestSemaphores.GetOrAdd(chunkPosition, _ => new SemaphoreSlim(1, 1));
         await semaphore.WaitAsync(cancellationToken);
 
--- VoxelWorld.Core/WorldLoaders/IChunkLoader.cs
+++ VoxelWorld.Core/WorldLoaders/IChunkLoader.cs
@@ -1,7 +1,13 @@
 namespace VoxelWorld.Core.WorldLoaders;
 
 
+/// <summary>
+/// 비동기 청크 로딩을 추상화하여 제공하는 인터페이스
+/// </summary>
 public interface IChunkLoader
 {
+    /// <summary>
+    /// 대상 좌표의 청크 클래스를 비동기로 로딩하여 반환합니다.
+    /// </summary>
     Task<Chunk> LoadChunkAsync(Vector3Int chunkPosition, CancellationToken cancellationToken = default);
 }
