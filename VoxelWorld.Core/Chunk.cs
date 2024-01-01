1a6e7964ee846c2ab40aba130e8735f6a29728e0
longZam <longzam305@gmail.com>
2024-01-01 23:17:32 +0900

    feat: Chunk 클래스의 스레드 안전성 보장과 병렬 처리 지원

--- VoxelWorld.Core/Chunk.cs
+++ VoxelWorld.Core/Chunk.cs
@@ -13,27 +13,46 @@ public class Chunk
     public const int VOLUME = CORNER * CORNER * CORNER;
 
     private readonly ushort[] rawData;
-    public bool initialized;
+    private readonly ReaderWriterLockSlim rwlock;
+    private bool initialized;
 
 
-    public ushort this[Vector3Int position]
+    /// <summary>
+    /// <para>
+    /// 해당 LocalPosition의 블럭을 읽고 쓸 수 있습니다.
+    /// </para>
+    /// <para>
+    /// 스레드로부터 안전하기 위해 매 접근마다 lock을 사용합니다.
+    /// 병렬로 동시에 읽거나 쓰려면 ParallelRead, ParallelWrite 함수의 사용을 고려하십시오.
+    /// </para>
+    /// </summary>
+    public ushort this[in Vector3Int localPosition]
     {
         get
         {
-            Debug.Assert(0 <= position.x && position.x < CORNER, $"x must be 0 <= x < {CORNER}, x = {position.x}");
-            Debug.Assert(0 <= position.y && position.y < CORNER, $"y must be 0 <= y < {CORNER}, y = {position.y}");
-            Debug.Assert(0 <= position.z && position.z < CORNER, $"z must be 0 <= z < {CORNER}, z = {position.z}");
+            rwlock.EnterReadLock();
 
-            return rawData[position.x + CORNER * position.y + SIDE * position.z];
+            try
+            {
+                return rawData[LocalPositionToIndex(localPosition)];
+            }
+            finally
+            {
+                rwlock.ExitReadLock();
+            }
         }
-
         set
         {
-            Debug.Assert(0 <= position.x && position.x < CORNER, $"x must be 0 <= x < {CORNER}, x = {position.x}");
-            Debug.Assert(0 <= position.y && position.y < CORNER, $"y must be 0 <= y < {CORNER}, y = {position.y}");
-            Debug.Assert(0 <= position.z && position.z < CORNER, $"z must be 0 <= z < {CORNER}, z = {position.z}");
+            rwlock.ExitWriteLock();
 
-            rawData[position.x + CORNER * position.y + SIDE * position.z] = value;
+            try
+            {
+                rawData[LocalPositionToIndex(localPosition)] = value;
+            }
+            finally
+            {
+                rwlock.ExitWriteLock();
+            }
         }
     }
 
@@ -41,7 +60,8 @@ public class Chunk
     public Chunk(bool clear = false)
     {
         this.rawData = ArrayPool<ushort>.Shared.Rent(CORNER * CORNER * CORNER);
-        
+        this.rwlock = new();
+
         if (clear)
             Array.Clear(this.rawData, 0, VOLUME);
     }
@@ -53,37 +73,147 @@ public class Chunk
 
     public void Serialize(BinaryWriter binaryWriter)
     {
-        binaryWriter.Write(initialized);
+        rwlock.EnterReadLock();
 
-        if (initialized)
-            for (int i = 0; i < VOLUME; i++)
-                binaryWriter.Write(rawData[i]);
+        try
+        {
+            binaryWriter.Write(initialized);
+
+            if (initialized)
+                for (int i = 0; i < VOLUME; i++)
+                    binaryWriter.Write(rawData[i]);
+        }
+        finally
+        {
+            rwlock.ExitReadLock();
+        }
     }
 
     public void Deserialize(BinaryReader binaryReader)
     {
-        initialized = binaryReader.ReadBoolean();
+        rwlock.EnterWriteLock();
 
-        if (initialized)
-            for (int i = 0; i < VOLUME; i++)
-                rawData[i] = binaryReader.ReadUInt16();
+        try
+        {
+            initialized = binaryReader.ReadBoolean();
+
+            if (initialized)
+                for (int i = 0; i < VOLUME; i++)
+                    rawData[i] = binaryReader.ReadUInt16();
+        }
+        finally
+        {
+            rwlock.ExitWriteLock();
+        }
     }
 
     public void Serialize(LoadChunkResponse response)
     {
-        response.Initialized = initialized;
+        rwlock.EnterReadLock();
 
-        if (initialized)
-            for (int i = 0; i < VOLUME; i++)
-                response.RawData.Add(rawData[i]);
+        try
+        {
+            response.Initialized = initialized;
+
+            if (initialized)
+                for (int i = 0; i < VOLUME; i++)
+                    response.RawData.Add(rawData[i]);
+        }
+        finally
+        {
+            rwlock.ExitReadLock();
+        }
     }
-    
+
     public void Deserialize(LoadChunkResponse response)
     {
-        initialized = response.Initialized;
+        rwlock.EnterWriteLock();
 
-        if (initialized)
-            for (int i = 0; i < VOLUME; i++)
-                rawData[i] = (ushort)response.RawData[i];
+        try
+        {
+            initialized = response.Initialized;
+
+            if (initialized)
+                for (int i = 0; i < VOLUME; i++)
+                    rawData[i] = (ushort)response.RawData[i];
+        }
+        finally
+        {
+            rwlock.ExitWriteLock();
+        }
+    }
+
+    public void CopyRawData(Span<ushort> destination)
+    {
+        if (destination.Length != VOLUME)
+            throw new ArgumentException($"Buffer size must be exactly {VOLUME}.", nameof(destination));
+        
+        rwlock.EnterReadLock();
+
+        try
+        {
+            rawData.CopyTo(destination);
+        }
+        finally
+        {
+            rwlock.ExitReadLock();
+        }
+    }
+
+    public delegate void ParallelReadAction(in Vector3Int position, in ushort block);
+    public delegate void ParallelWriteAction(in Vector3Int position, ref ushort block);
+
+    public void ParallelRead(ParallelReadAction action)
+    {
+        rwlock.EnterReadLock();
+
+        try
+        {
+            Parallel.For(0, VOLUME, i =>
+            {
+                action(IndexToLocalPosition(i), in rawData[i]);
+            });
+        }
+        finally
+        {
+            rwlock.ExitReadLock();
+        }
+    }
+
+    public void ParallelWrite(ParallelWriteAction action)
+    {
+        rwlock.EnterWriteLock();
+
+        try
+        {
+            Parallel.For(0, VOLUME, i =>
+            {
+                action(IndexToLocalPosition(i), ref rawData[i]);
+            });
+        }
+        finally
+        {
+            rwlock.ExitWriteLock();
+        }
+    }
+
+    private static int LocalPositionToIndex(in Vector3Int localPosition)
+    {
+        Debug.Assert(0 <= localPosition.x && localPosition.x < CORNER, $"x must be 0 <= x < {CORNER}, x = {localPosition.x}");
+        Debug.Assert(0 <= localPosition.y && localPosition.y < CORNER, $"y must be 0 <= y < {CORNER}, y = {localPosition.y}");
+        Debug.Assert(0 <= localPosition.z && localPosition.z < CORNER, $"z must be 0 <= z < {CORNER}, z = {localPosition.z}");
+
+        return localPosition.x + CORNER * localPosition.y + SIDE * localPosition.z;
+    }
+
+    private static Vector3Int IndexToLocalPosition(in int index)
+    {
+        Debug.Assert(0 <= index && index < VOLUME, $"index must be 0 <= index < {VOLUME}, index = {index}");
+
+        return new(
+            index % CORNER,
+            index / CORNER % CORNER,
+            index / SIDE % CORNER
+        );
     }
 }
